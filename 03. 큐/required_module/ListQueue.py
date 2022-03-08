65adea9edc7f62fcfdf185c733a08d8b8ec89805
uk1996 <csuk1947@gmail.com>
2022-03-08 10:12:21 +0900

    update 큐/required_module/ListQueue, ListStack

---
+++ 03. 큐/required_module/ListQueue.py
@@ -0,0 +1,24 @@
+class ListQueue:
+    def __init__(self):
+        self.__queue = []
+    
+    def enqueue(self, x):
+        self.__queue.append(x)
+    
+    def dequeue(self):
+        return self.__queue.pop(0)
+    
+    def front(self):
+        return self.__queue[0]
+    
+    def isEmpty(self):
+        return len(self) == 0
+    
+    def dequeueAll(self):
+        self.__queue.clear()
+    
+    def printQueue(self):
+        print('Queue from front:', end = ' ')
+        for value in self.__queue:
+            print(value, end = ' ')
+        print()
---
+++ 03. 큐/required_module/ListStack.py
@@ -0,0 +1,27 @@
+class ListStack:
+    def __init__(self):
+        self.__stack = []
+    
+    def push(self, x):
+        self.__stack.append(x)
+    
+    def pop(self):
+        return self.__stack.pop()
+    
+    def top(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.__stack[-1]
+    
+    def isEmpty(self):
+        return not bool(self.__stack)
+    
+    def popAll(self):
+        self.__stack = []
+
+    def printStack(self):
+        print('Stack from top:', end=' ')
+        for i in range(len(self.__stack) - 1, -1, -1):
+            print(self.__stack[i], end=' ')
+        print()
